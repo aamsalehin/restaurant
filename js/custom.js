48f9dd1014f39ac34942d29cf2bd17a4d3d8e03e
aamsalehin <65489361+aamsalehin@users.noreply.github.com>
2020-06-21 23:26:43 +0600

    restaurent

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function(){
+  
+   $(".abc").focus(function(){
+    $(this).css({"background-color": "transparent", "color":"#84868a", "border":"2px #15a4fa solid"});
+  }); 
+    
+    
+    
+    
+    
+    
+});
